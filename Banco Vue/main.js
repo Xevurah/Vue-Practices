455fe4af0f51afa0cdc3c6f5c902e4ceafcb5f4b
Xevurah <ali_524-@hotmail.com>
2021-07-31 04:24:54 -0500

    Banco vue

--- Banco Vue/main.js
+++ Banco Vue/main.js
@@ -1,11 +1,36 @@
 const app = Vue.createApp({
+    // objeto que bindea
     data(){
         return{
             titulo: 'Mi banco con Vue.js',
-            cantidad: 0,
+            cantidad: 1000,
             enlace: 'https://youtube.com',
             estado: true,
-            servicios: ['transferencias','pagos','giros','cheques']
+            servicios: ['transferencias','pagos','giros','cheques'],
+            desactivar:false
+        }
+    },
+    // metodos
+    methods:{
+        agregarSaldo(){
+            this.cantidad = this.cantidad + 100
+        },
+        disminuirSaldo(valor){
+            if(this.cantidad===0){
+                alert('saldo en cero!')
+                this.desactivar = true
+                return
+            }
+            this.cantidad = this.cantidad - valor
+        }
+    },
+    // propiedades computadas
+    computed:{
+        colorCantidad(){
+            return this.cantidad > 500 ? 'text-success' : 'text-danger'
+        },
+        mayusculasTexto(){
+            return this.titulo.toUpperCase()
         }
     }
 })
