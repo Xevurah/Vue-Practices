9f7b7792fa2c9814029203028720a4a15f624e40
Xevurah <ali_524-@hotmail.com>
2021-08-07 00:17:35 -0500

    Vuex vue backend

---
+++ Vue Cli/vuex-1/src/components/BotonAccion.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="accionBoton({ estado: estado, numero: 10 })">
+    {{ textoBoton }}
+  </button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  props: {
+    estado: Boolean,
+  },
+  computed: {
+    textoBoton() {
+      return this.estado ? "Aumentar" : "Diminuir";
+    },
+  },
+  methods: {
+    ...mapActions(["accionBoton"]),
+  },
+};
+</script>
+
+<style></style>
---
+++ Vue Cli/vuex-1/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1 :style="colorContador">{{ titulo }}: {{ contador }}</h1>
+    <button @click="accionIncrementar">Aumentar</button>
+    <BtnDisminuir />
+
+    <hr />
+    <BotonAccion :estado="true" />
+    <BotonAccion :estado="false" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BtnDisminuir from "../components/BtnDisminuir.vue";
+import BotonAccion from "../components/BotonAccion.vue";
+import { mapState, mapMutations, mapActions } from "vuex";
+export default {
+  name: "Home",
+  components: {
+    BtnDisminuir,
+    BotonAccion,
+  },
+  data() {
+    return {
+      titulo: "Mi contador Vuex",
+    };
+  },
+  computed: {
+    ...mapState(["contador"]),
+    colorContador() {
+      return [this.contador > 99 ? { color: "green" } : { color: "red" }];
+    },
+  },
+  methods: {
+    ...mapMutations(["incrementar"]),
+    ...mapActions(["accionIncrementar"]),
+  },
+};
+</script>
